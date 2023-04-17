08d60cbf915a26ab554defa410511c600570e8a2
HenriqueSilva367 <jhenrique367@gmail.com>
2023-04-17 01:37:33 -0300

    Parte do login e Token já esta funcionando,  falta a parte de recuperação de senhas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [PrismaModule, UserModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: JwtAuthGuard,
+    // },
+  ],
 })
 export class AppModule {}
